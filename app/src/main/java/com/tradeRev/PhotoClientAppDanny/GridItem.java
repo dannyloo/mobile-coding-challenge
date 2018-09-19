809364720088bf9421571779cfda2c9a47aa68bc
Danny Loo <dannyloo2005@hotmail.com>
2018-09-19 17:45:00 -0400

    Project created. Main functionality tested working, queries JSON for image url and details. Images shown in grid view.

---
+++ app/src/main/java/com/tradeRev/PhotoClientAppDanny/GridItem.java
@@ -0,0 +1,31 @@
+package com.tradeRev.PhotoClientAppDanny;
+
+public class GridItem {
+    private String image;
+    private String imageRaw;
+    private String title;
+
+    public GridItem() {
+        super();
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setImageRaw(String imageRaw){ this.imageRaw = imageRaw; }
+
+    public String getImageRaw(){ return imageRaw; }
+}
